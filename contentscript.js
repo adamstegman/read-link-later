33390a3cb6ac7ad9d677e01753014fc205afa7ca
Adam Stegman <astegman@gmail.com>
2010-08-05 01:22:05 -0500

    Implemented prependReadLaterLinkTo, and added onclick handler and addToInstapaper callback. Actual script adds the links to the tweets.

--- contentscript.js
+++ contentscript.js
@@ -54,5 +54,47 @@ function findActionsForTweetsWithUrls() {
  *                           list item to.
  */
 function prependReadLaterLinkTo(listElement) {
+  // Create the Read Later link
+  var link = document.createElement('a');
+  link.href = "#";
+  link.title = "Add to Instapaper";
+  link.appendChild(document.createTextNode("Read Later"));
+  link.onclick = sendAddToInstapaperRequest;
   
+  var span = document.createElement('span');
+  span.className = "read-tweet-later";
+  span.appendChild(link);
+  
+  var li = document.createElement('li');
+  li.insertBefore(span, li.firstChild);
+}
+
+/**
+ * Sends the request to the extension to add the URL to Instapaper.
+ *
+ * @param {Event} event The Event object sent by the onclick event.
+ */
+function sendAddToInstapaperRequest(event) {
+  chrome.extension.sendRequest({'action' : 'addToInstapaper',
+                                'url' : "http://www.google.com/", // TODO
+                                'element' : event.target},
+                               onInstapaperReturn);
+}
+
+/**
+ * Handles the Instapaper return status. On success, changes the display of
+ * the "Read Later" link to notify the user it worked. On failure, alerts the
+ * user.
+ *
+ * @param {Integer} status The HTTP status Instapaper returned.
+ * @param {HTMLElement} element The HTMLElement to modify based on status.
+ */
+function onInstapaperReturn(status, element) {
+  // TODO
+}
+
+// Prepend a "Read Later" link to the action lists of tweets with URLs.
+actionLists = findActionsForTweetsWithUrls();
+for (list in actionLists) {
+  prependReadLaterLinkTo(list);
 }
