c3c05a347f09bd36ede1cc2cfccdd7599ede2136
Adam Stegman <astegman@gmail.com>
2010-08-08 23:28:39 -0500

    Removed findActions... function; it's done in the normal script now. addToInstapaper's request slightly modified. id added to each link to avoid JSONifying an entire HTMLElement.

--- contentscript.js
+++ contentscript.js
@@ -1,45 +1,14 @@
 /**
- * Parses the page's DOM for tweet elements containing URLs and returns an
- * Array of their action lists. Each element of the return Array can have a
- * "Read Later" link inserted.
- *
- * @return Array of Node, ul.actions-hover, for tweets with URLs.
- */
-function findActionsForTweetsWithUrls() {
-  var timeline = document.getElementById("timeline");
-  if (!timeline) return; // need a timeline
-  var actionsForStatusesWithUrls = new Array();
-  var statuses = timeline.getElementsByClassName('status');
-  var statusesLength = statuses.length;
-  for (var s = 0; s < statusesLength; s++) {
-    var status = statuses[s];
-    
-    // Find the status body
-    var statusBody = status.getElementsByClassName('status-body');
-    if (!statusBody.length) continue;
-    statusBody = statusBody[0];
-    
-    // TODO: Skip the status if it doesn't have a URL
-    
-    // Find the actions hover list (Reply, Retweet)
-    var actionsHoverList = statusBody.getElementsByClassName('actions-hover');
-    if (!actionsHoverList.length) continue;
-    actionsHoverList = actionsHoverList[0];
-    
-    actionsForStatusesWithUrls.push(actionsHoverList);
-  }
-  return actionsForStatusesWithUrls;
-}
-
-/**
  * Prepends a list item with a "Read Later" link to the given list element.
  *
  * @param {HTMLElement} listElement The ol or ul element to prepend the "Read
  *                                  Later" list item to.
+ * @param {Integer} linkId (Optional) An id to add to the link.
  */
-function prependReadLaterLinkTo(listElement) {
+function prependReadLaterLinkTo(listElement, linkId) {
   // Create the Read Later link
   var link = document.createElement('a');
+  if (linkId) link.id = linkId;
   link.href = "#";
   link.title = "Add to Instapaper";
   link.appendChild(document.createTextNode("Read Later"));
@@ -61,10 +30,11 @@ function prependReadLaterLinkTo(listElement) {
  * @param {Event} event The Event object sent by the onclick event.
  */
 function sendAddToInstapaperRequest(event) {
-  chrome.extension.sendRequest({'action' : 'addToInstapaper',
-                                'url' : "http://www.google.com/", // TODO
-                                'element' : event.target},
+  chrome.extension.sendRequest({'action': 'addToInstapaper',
+                                'readLaterUrl': 'http://www.google.com/', // TODO
+                                'senderId': event.target.id},
                                onInstapaperReturn);
+  event.preventDefault();
 }
 
 /**
@@ -72,14 +42,24 @@ function sendAddToInstapaperRequest(event) {
  * the "Read Later" link to notify the user it worked. On failure, alerts the
  * user.
  *
- * @param {Integer} status The HTTP status Instapaper returned.
- * @param {HTMLElement} element The HTMLElement to modify based on status.
+ * @param {Object} response The response object sent by the event handler.
  */
-function onInstapaperReturn(status, element) {
-  // TODO
+function onInstapaperReturn(response) {
+  console.log("response", response);
 }
 
 // Prepend a "Read Later" link to the action lists of tweets with URLs.
-findActionsForTweetsWithUrls().forEach(function (list) {
-  prependReadLaterLinkTo(list);
-});
+var tweets = document.getElementsByClassName('status-body'),
+    tweetsLength = tweets.length;
+for (var i = 0; i < tweetsLength; i++) {
+  var statusBody = tweets[i];
+  // TODO: Skip the status if it doesn't have a URL
+  
+  // Find the actions hover list (Reply, Retweet)
+  var actionsHoverList = statusBody.getElementsByClassName('actions-hover');
+  if (!actionsHoverList.length) continue;
+  actionsHoverList = actionsHoverList[0];
+  
+  prependReadLaterLinkTo(actionsHoverList,
+                         statusBody.parentNode.id.replace('status', 'read-tweet-later'));
+}
